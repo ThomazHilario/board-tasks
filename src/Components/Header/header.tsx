822c447ec00c6c438343cf4227111a1a9f7abc99
Thomaz Alves <thomazhilario5@gmail.com>
2025-07-23 15:19:27 -0300

    feat: Create UI to user logged and user not logged.

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -2,7 +2,8 @@
 
 // Components
 import Link from 'next/link'
-import { signIn } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Image from 'next/image'
 
 // Dialog Components
 import { 
@@ -22,6 +23,7 @@ import { IoLogoDiscord } from "react-icons/io5";
 import style from './header.module.css'
 
 export const Header = () => {
+    const { data } = useSession()
 
     return (
         <header className={style.header}>
@@ -39,19 +41,46 @@ export const Header = () => {
                 
                 <DialogPortal>
                     <DialogContent className={style.dialogContent} aria-describedby='testes'>
-                        <DialogHeader className={style.dialogHeader}>
-                            <DialogTitle className={style.dialogTitle}>Registrar-se</DialogTitle>
-                            <DialogDescription hidden>Dialog de login ou registro do usuário</DialogDescription>
-                            
-                            {/* Account login */}                               
-                                <button 
-                                    className={style.buttonEnterInMyAccount}
-                                    onClick={() => signIn('discord')}
-                                >
-                                    <IoLogoDiscord size={25} color='#7289da'/>
-                                    Entrar com Discord
-                                </button>
-                        </DialogHeader>
+                        {data?.user ? (
+                            <DialogHeader className={style.dialogHeaderProfile}>
+                                <DialogTitle className={style.dialogTitleProfile}>
+                                    <img 
+                                        className={style.imageProfile}
+                                        src={data.user.image!} 
+                                        alt="Imagem de perfil" 
+                                        width={50} 
+                                        height={50}
+                                    />
+                                    {data.user.name}
+                                </DialogTitle>
+
+                                <DialogDescription className={style.dialogLinksProfileContainer}>
+                                    <Link 
+                                        href='board' 
+                                        className={style.linkProfile}
+                                    >
+                                        Minhas Tarefas
+                                    </Link>
+                                    <button className={style.buttonSignOut} onClick={() => signOut()}>
+                                        Sair
+                                    </button>
+                                </DialogDescription>
+                            </DialogHeader>
+                        ): (
+                            <DialogHeader className={style.dialogHeader}>
+                                <DialogTitle className={style.dialogTitle}>Registrar-se</DialogTitle>
+                                <DialogDescription hidden>Dialog de login ou registro do usuário</DialogDescription>
+                                
+                                {/* Account login */}                               
+                                    <button 
+                                        className={style.buttonEnterInMyAccount}
+                                        onClick={() => signIn('discord')}
+                                    >
+                                        <IoLogoDiscord size={25} color='#7289da'/>
+                                        Entrar com Discord
+                                    </button>
+                            </DialogHeader>
+                        )}
                     </DialogContent>
                 </DialogPortal>
             </Dialog>
